9539e281881403f0f5a837d06d6cd2c580fb29df
renamartha <renamartha05@gmail.com>
2023-11-21 08:11:02 +0700

    untuk tugas 9

--- kelontong/urls.py
+++ kelontong/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
 ]
